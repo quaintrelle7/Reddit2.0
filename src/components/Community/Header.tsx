62d96c850686d084f063d4427d2e1775e17a878f
Sharayu <sharayushendre7@gmail.com>
2023-04-17 23:16:05 +0530

    mondayyy

--- src/components/Community/Header.tsx
+++ src/components/Community/Header.tsx
@@ -2,14 +2,17 @@ import { Community } from '@/atoms/communitiesAtom';
 import { Box, Button, Flex, Icon, Image, Text } from '@chakra-ui/react';
 import React from 'react';
 import { FaReddit } from 'react-icons/fa';
-
+import useCommunityData from '@/hooks/useCommunityData';
 
 type HeaderProps = {
     communityData: Community;
 };
 
 const Header: React.FC<HeaderProps> = ({ communityData }) => {
-    const isJoined = false; //read from communitySnippets
+
+    const { communityStateValue, onJoinOrLeaveCommunity } = useCommunityData();
+    const isJoined = !!communityStateValue.mySnippets.find
+        ((item) => item.communityId === communityData.id); //read from communitySnippets
 
     return (
         <Flex direction="column" width="100%" height="146px">
@@ -39,7 +42,7 @@ const Header: React.FC<HeaderProps> = ({ communityData }) => {
                         </Flex>
                         <Button
                             variant={isJoined ? "outline" : "solid"}
-                        height="30px" px={6} onClick={()=>{}}>{isJoined ? "Joined" : "Join"}</Button>
+                            height="30px" px={6} onClick={() => onJoinOrLeaveCommunity(communityData, isJoined)}>{isJoined ? "Joined" : "Join"}</Button>
                     </Flex>
 
                 </Flex>
---
+++ src/hooks/useCommunityData.tsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { Community, CommunityState } from '@/atoms/communitiesAtom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth, firestore } from '@/firebase/clientApp';
+import { getDocs, collection } from 'firebase/firestore';
+
+
+const useCommunityData = () => {
+
+    const [communityStateValue, setCommunityStateValue] =
+        useRecoilState(CommunityState);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState('');
+    const [user] = useAuthState(auth);
+
+    const onJoinOrLeaveCommunity = (communityData: Community, isJoined: boolean) => {
+        //Is the user signed In?
+        //If not => Open Auth model
+        if (isJoined) {
+            leaveCommuntiy(communityData.id);
+            return;
+        }
+        joinCommunity(communityData);
+
+    };
+
+    const getMySnippets = async () => {
+        setLoading(true);
+
+        try {
+            //get users snippets
+            const snippetDocs = await getDocs
+                (collection(firestore, `users/${user?.uid}/communities`));
+
+            const snippets = snippetDocs.docs.map(doc => ({ ...doc.data() }));
+            console.log("here are snippets", snippets);
+
+        } catch (error) {
+            console.log("getMySnippets Error", error);
+        }
+    }
+    const joinCommunity = (communityData: Community) => { };
+    const leaveCommuntiy = (communityId: string) => { };
+
+    //the below useEffect hook has dependency in the user, everytime 
+    //the user chanes, it'll render and so will call the getSnippets function
+    
+    useEffect(()=>{
+
+        if(!user) return;
+        getMySnippets();
+    },[user])
+    return {
+        communityStateValue,
+        onJoinOrLeaveCommunity,
+    }
+}
+export default useCommunityData;
